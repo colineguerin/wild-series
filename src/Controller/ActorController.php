ff916e71c742b78505677e3098df95903746bee2
colineguerin <guerin.coline@gmail.com>
2023-06-01 13:36:04 +0200

    quest19: actors can be added to new program

--- src/Controller/ActorController.php
+++ src/Controller/ActorController.php
@@ -6,13 +6,20 @@ use App\Entity\Actor;
 use App\Repository\ActorRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 #[Route('/actor', name: 'actor_')]
 class ActorController extends AbstractController
 {
+    #[Route('/', name: 'index')]
+    public function index(ActorRepository $actorRepository): Response
+    {
+        $actors = $actorRepository->findAll();
+
+        return $this->render('actor/index.html.twig', [
+            'actors' => $actors,
+        ]);
+    }
     #[Route('/{id}', name: 'show', requirements: ['id' => '\d+'], methods: ['GET'])]
     public function show(Actor $actor): Response
     {
--- src/Form/ProgramType.php
+++ src/Form/ProgramType.php
@@ -2,7 +2,9 @@
 
 namespace App\Form;
 
+use App\Entity\Actor;
 use App\Entity\Program;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -17,6 +19,13 @@ class ProgramType extends AbstractType
             ->add('synopsis', TextType::class)
             ->add('poster', TextType::class)
             ->add('category', null, ['choice_label' => 'name'])
+            ->add('actors', EntityType::class, [
+                'class' => Actor::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => true,
+                'by_reference' => false,
+            ]);
         ;
     }
 
